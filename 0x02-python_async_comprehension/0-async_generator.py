919924a6fc40bee01268a7527c9a676fd93d6e6f
Iadekunle3 <Iadekunle34@gmail.com>
2024-06-11 12:35:20 -0400

    async comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module contain a coroutine function """
+
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """coroutine that loops 10 times, wait 1 sec each time"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
